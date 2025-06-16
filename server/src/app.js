0cc303c436d3237e106490cdf33655e60bff4dd0
nitinsahu19 <sahunitin900@gmail.com>
2025-06-17 04:03:03 +0530

    error handling

--- server/src/app.js
+++ server/src/app.js
@@ -3,18 +3,17 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-// middlewares
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
-
-// Controller for  admin
-app.post("/admin/deleteData", (req, res) => {
-  res.send("Data deleted successfully");
-});
-
 // Controller for user
 app.get("/user/details", (req, res) => {
+  throw new Error("Demo error");
   res.json({ name: "Nitin Sahu", age: 25 });
+});
+
+// Error handling route or simple we can use try catch in all the routes for better approach
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Internal server error!!");
+  }
 });
 
 app.listen(3000, () => console.log("Server is running at 3000"));
