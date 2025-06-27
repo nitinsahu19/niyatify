c2ce6dad169f51e92c97e22074e125eff19a12ce
nitinsahu19 <sahunitin900@gmail.com>
2025-06-27 06:46:13 +0530

    api lever data sanitization

--- server/src/app.js
+++ server/src/app.js
@@ -6,31 +6,47 @@ const User = require("./models/User");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.send("User saved successfully!!");
+    res.status(201).send("User saved successfully!!");
   } catch (error) {
     res.status(400).send("Error in savind the user" + error.message);
     console.log(error.message);
   }
 });
 
-app.patch("/user", async (req, res) => {
+app.patch("/user/:userId", async (req, res) => {
   try {
-    const userId = req.body.userId;
+    const userId = req.params.userId;
     const data = req.body;
-    console.log(data, userId);
+
+    const ALLOWED_UPDATES = ["firstName", "lastName", "skills"];
+
+    const isAllowed = Object.keys(data).every((key) =>
+      ALLOWED_UPDATES.includes(key)
+    );
+    console.log(data);
+
+    if (!isAllowed) {
+      throw new Error(
+        "User updation failed due to updation of unallowed fields "
+      );
+    }
+
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills can maximum 10 only");
+    }
+
     await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
     });
+
     res.status(201).send("User updated successfully");
   } catch (error) {
-    res.status(400).send("Updated failed: ", error.message);
-    console.log(error.message);
+    res.status(400).send("Updated failed: " + error.message);
   }
 });
 
